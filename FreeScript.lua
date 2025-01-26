5e38e9ff84a72fa8dae5e2a24c8f41e02f3c6394
HeiusRoblox <hieudz5913@gmail.com>
2025-01-26 20:31:12 +0700

    Update FreeScript.lua

--- FreeScript.lua
+++ FreeScript.lua
@@ -1,23 +1,3 @@
--- Tạo một đối tượng ScreenGui để chứa nút và hiển thị trên màn hình của người chơi
-local screenGui = Instance.new("ScreenGui")
-screenGui.Parent = game:GetService("Players").LocalPlayer.PlayerGui
-
--- Tạo một đối tượng TextButton (nút văn bản) để đại diện cho nút
-local button = Instance.new("TextButton")
-button.Size = UDim2.new(0, 80, 0, 30) -- Kích thước của nút (80x30 pixels)
-button.Position = UDim2.new(0, 10, 0, 10) -- Vị trí của nút (10 pixels từ góc trái và trên)
-button.BackgroundColor3 = Color3.fromRGB(255, 255, 255) -- Màu trắng
-
--- Văn bản trên nút
-button.Text = "Stop Tween"
-button.Parent = screenGui -- Đặt nút là con của ScreenGui
-
--- Gán sự kiện khi nút được nhấn
-button.MouseButton1Click:Connect(function()
-    -- Điều khiển hành động khi nút được nhấn ở đây
-    toTarget(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame)
-end)
-
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
